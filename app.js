9b17ce92fbb077d53ac4fc314ceb5b3dc703483f
jazzcort <jason101011113@gmail.com>
2023-09-19 18:14:57 -0400

    ready to deploy

--- app.js
+++ app.js
@@ -151,6 +151,7 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err })
 })
 
+const port = process.env.PORT || 3000
 app.listen(3000, () => {
-    console.log("Listining on port 3000")
+    console.log(`Listining on port ${port}`)
 })
